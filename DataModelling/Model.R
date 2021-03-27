e709cf223ef41d7977a5e52c24b8d73480d543fd
MikkelGraugaard <Mikkel21-97@hotmail.com>
2021-03-27 15:39:33 +0100

    Starting on Data Models

---
+++ DataModelling/Model.R
@@ -0,0 +1,85 @@
+# Data Modelling Mikkel
+
+
+# Load data
+
+
+
+
+
+# Explore data
+
+
+
+
+# Base naive predictions
+
+
+
+
+
+# Logistic model
+
+
+
+
+
+
+
+# Support Vector machines
+
+
+
+
+
+
+
+
+
+# Random Forest
+
+
+
+
+
+
+
+
+
+
+
+
+# Extreme Gradient Boosting 
+
+
+
+
+
+
+
+
+
+
+# Neural Networks
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

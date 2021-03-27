1487767351100c9d70b14feee3c5d4069b68817d
Mikkel Graugaard <79583068+MikkelGraugaard@users.noreply.github.com>
2021-03-27 15:50:12 +0100

    Update Model_Mikkel.R

--- DataModelling/Model_Mikkel.R
+++ DataModelling/Model_Mikkel.R
@@ -1,8 +1,17 @@
 # Data Modelling Mikkel
 
+# Load libraries
+library("readr")
+library("lubridate")
+
 
 # Load data
+gme <- read.delim("~/R-data/Data Science Project/PredictStockusingRedditandTwitter/PreProcessedData/GME_merged.csv", sep="," )
 
+
+# Explore data
+str(gme)
+gme$created_utc <- as.POSIXct(gme$created_utc)
 
 
 
